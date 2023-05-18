954ce4e29ca3d7b0a8f559e8865d29f823ce31f9
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-18 12:50:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 function calculateMinCost() {
   //your code here
-  const input = document.getElementById("rope-lengths").value;
 
-	const arr = input.split(",");
+	const input = document.getElementById('rope-lengths');
 
-	const heap = new Heap();
+	const arr = input.split(',');
+	console.log(arr);
+	
+ //  const input = document.getElementById("rope-lengths").value;
 
+	// const arr = input.split(",");
 
-	for(const lenght of arr){
-		heap.add(lenght);
-	}
-
-	let cost = 0;
-
-	while (!heap.isEmpty()) {
-		const rope1 = heap.remove();
-		const rope2 = heap.remove();
-
-		cost += rope1 + rope2;
+	// const heap = new Heap();
 
 
-		heap.add(rope1 + rope2);
-	}
+	// for(const lenght of arr){
+	// 	heap.add(lenght);
+	// }
 
-	document.getElementById("result").innerHTML = cost;
+	// let cost = 0;
+
+	// while (!heap.isEmpty()) {
+	// 	const rope1 = heap.remove();
+	// 	const rope2 = heap.remove();
+
+	// 	cost += rope1 + rope2;
+
+
+	// 	heap.add(rope1 + rope2);
+	// }
+
+	// document.getElementById("result").innerHTML = cost;
 }  
